4e2348d09bcc605c834546ae522069d62a336bc6
Fernirx <anhtuhungdeveloper@gmail.com>
2026-03-07 14:11:30 +0700

    create spring project

---
+++ src/main/java/vn/fernirx/clothes/ClothesStoreApplication.java
@@ -0,0 +1,13 @@
+package vn.fernirx.clothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClothesStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/fernirx/clothes/ClothesStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.fernirx.clothes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
