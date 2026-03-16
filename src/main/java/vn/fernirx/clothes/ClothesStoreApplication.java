6a46049e9c56fb2052a145296712078dbeaac25f
Fernirx <anhtuhungdeveloper@gmail.com>
2026-03-16 23:47:01 +0700

    chore(app): bật ConfigurationPropertiesScan trên class khởi động
    Thêm @ConfigurationPropertiesScan để Spring tự động quét và đăng ký
    tất cả các class @ConfigurationProperties trong classpath, thay vì phải
    khai báo thủ công từng class bằng @EnableConfigurationProperties.

--- src/main/java/vn/fernirx/clothes/ClothesStoreApplication.java
+++ src/main/java/vn/fernirx/clothes/ClothesStoreApplication.java
@@ -1,9 +1,12 @@
 package vn.fernirx.clothes;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ClothesStoreApplication {
 
     public static void main(String[] args) {
