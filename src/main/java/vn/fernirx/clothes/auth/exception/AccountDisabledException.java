dd9d0ab2e77071f474bd95800b1b3d18c52dd0b3
Fernirx <anhtuhungdeveloper@gmail.com>
2026-04-10 17:33:06 +0700

    fix(auth): sửa indentation sai trong AccountDisabledException
    Class declaration bị thêm nhầm indent thừa ở commit trước.

--- src/main/java/vn/fernirx/clothes/auth/exception/AccountDisabledException.java
+++ src/main/java/vn/fernirx/clothes/auth/exception/AccountDisabledException.java
@@ -3,7 +3,7 @@ package vn.fernirx.clothes.auth.exception;
 import vn.fernirx.clothes.common.enums.ErrorCode;
 import vn.fernirx.clothes.common.exception.SecurityBaseException;
 
-    public class AccountDisabledException extends SecurityBaseException {
+public class AccountDisabledException extends SecurityBaseException {
     public AccountDisabledException() {
         super(ErrorCode.ACCOUNT_DISABLED, "Account is disabled");
     }
